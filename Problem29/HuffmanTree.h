2ba6f159b8df8805f29f3beb0561ecfa7562a107
Hoàng Mạnh Khiêm <khiemhm04@gmail.com>
2023-11-11 22:50:50 +0700

    update

--- Problem29/HuffmanTree.h
+++ Problem29/HuffmanTree.h
@@ -16,28 +16,27 @@ using namespace std;
 struct Node
 {
     char c;
-    int freq;  // tần suất
-    Node *left, *right; // con trỏ trái, phải
+    int freq;
+    Node *left, *right;
 };
 
 Node *newNode(char c, int freq, Node *left, Node *right) // tạo node mới
 {
-    Node *node = new Node(); // cấp phát động
-    node->c = c; // kí tự
-    node->freq = freq; // tần suất
-    node->left = left; // con trái
-    node->right = right; // con phải
-    return node; // trả về node
+    Node *node = new Node();
+    node->c = c;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+    return node;
 }
 
-struct comp // so sánh
+struct comp
 {
     bool operator()(Node *l, Node *r)
     {
         return l->freq > r->freq;
     }
 };
-
 void encode(Node *root, string str, map<char, string> &huffmanCode) // mã hóa
 {
     if (root == nullptr)
@@ -51,12 +50,6 @@ void encode(Node *root, string str, map<char, string> &huffmanCode) // mã hóa
     encode(root->left, str + "0", huffmanCode);
     encode(root->right, str + "1", huffmanCode);
 }
-
-//giải thích hàm encode: hàm encode sẽ duyệt cây huffman từ gốc đến lá,
-// mỗi khi đi qua một nút nào đó thì nó sẽ thêm vào chuỗi str
-// một bit 0 nếu đi qua nút trái và 1 nếu đi qua nút phải,
-// nếu nút đó là lá thì nó sẽ thêm vào map huffmanCode
-// một cặp key-value là kí tự và chuỗi str
 
 void decode(Node *root, int &index, string str) // giải mã
 {
@@ -79,13 +72,7 @@ void decode(Node *root, int &index, string str) // giải mã
         decode(root->right, index, str);
 }
 
-// giải thích hàm decode: hàm decode sẽ duyệt cây huffman từ gốc đến lá,
-// mỗi khi đi qua một nút nào đó thì nó sẽ tăng biến index lên 1,
-// nếu bit thứ index trong chuỗi str là 0 thì nó sẽ đi qua nút trái,
-// nếu bit thứ index trong chuỗi str là 1 thì nó sẽ đi qua nút phải,
-// nếu nút đó là lá thì nó sẽ in ra kí tự của nút đó
-
-void buildHuffmanTree(string text)
+void buildHuffmanTree_encode(string text)
 {
     map<char, int> freq;
     for (char c : text)
@@ -94,7 +81,6 @@ void buildHuffmanTree(string text)
     }
 
     priority_queue<Node *, vector<Node *>, comp> pq;
-
 
     for (auto pair : freq)
     {
@@ -117,25 +103,45 @@ void buildHuffmanTree(string text)
     map<char, string> huffmanCode;
     encode(root, "", huffmanCode);
 
-    cout << "Huffman Codes are :\n"
-         << '\n';
+    cout << "Huffman Codes are :\n";
     for (auto pair : huffmanCode)
     {
         cout << pair.first << " " << pair.second << '\n';
     }
-
-    cout << "\nOriginal string was :\n"
-         << text << '\n';
 
     string str;
     for (char c : text)
     {
         str += huffmanCode[c];
     }
+    cout << "\nEncoded string is :\n" << str << '\n';
+}
 
-    cout << "\nEncoded string is :\n"
-         << str << '\n';
+void buildHuffmanTree_decode(map<char, int> freq, string str)
+{
+    priority_queue<Node *, vector<Node *>, comp> pq;
 
+    for (auto pair : freq)
+    {
+        pq.push(newNode(pair.first, pair.second, nullptr, nullptr));
+    }
+
+    while (pq.size() != 1)
+    {
+        Node *left = pq.top();
+        pq.pop();
+        Node *right = pq.top();
+        pq.pop();
+
+        int sum = left->freq + right->freq;
+        pq.push(newNode('\0', sum, left, right));
+    }
+    cout << "Huffman Codes are :\n";
+    for (auto pair : freq)
+    {
+        cout << pair.first << " " << pair.second << '\n';
+    }
+    Node *root = pq.top();
     int index = -1;
     cout << "\nDecoded string is: \n";
     while (index < (int)str.size() - 2)
@@ -143,28 +149,5 @@ void buildHuffmanTree(string text)
         decode(root, index, str);
     }
 }
-
-// giải thích hàm buildHuffmanTree: hàm buildHuffmanTree sẽ tạo cây huffman,
-// đầu tiên nó sẽ tạo một map freq để lưu tần suất của các kí tự trong chuỗi text,
-// sau đó nó sẽ tạo một priority_queue pq để lưu các node,
-// mỗi node sẽ có các thuộc tính là kí tự, tần suất, con trái, con phải,
-// sau đó nó sẽ duyệt map freq để thêm các node vào priority_queue pq,
-// mỗi node sẽ có kí tự là key của map freq, tần suất là value của map freq,
-// con trái và con phải sẽ là nullptr,
-// sau đó nó sẽ lấy ra 2 node có tần suất nhỏ nhất trong priority_queue pq,
-// tạo một node mới có kí tự là '\0', tần suất là tổng tần suất của 2 node trên,
-// con trái là node có tần suất nhỏ nhất, con phải là node có tần suất nhỏ thứ 2,
-// sau đó nó sẽ thêm node mới này vào priority_queue pq,
-// lặp lại cho đến khi priority_queue pq chỉ còn 1 node,
-// node này chính là gốc của cây huffman,
-// sau đó nó sẽ tạo một map huffmanCode để lưu mã huffman của các kí tự,
-// sau đó nó sẽ gọi hàm encode để mã hóa cây huffman,
-// sau đó nó sẽ in ra mã huffman của các kí tự,
-// sau đó nó sẽ tạo một chuỗi str để lưu mã huffman của chuỗi text,
-// sau đó nó sẽ duyệt chuỗi text để lấy mã huffman của từng kí tự,
-// sau đó nó sẽ in ra chuỗi str,
-// sau đó nó sẽ tạo một biến index để lưu vị trí của bit trong chuỗi str,
-// sau đó nó sẽ gọi hàm decode để giải mã chuỗi str,
-// sau đó nó sẽ in ra chuỗi text đã được giải mã
 
 #endif //PROBLEM29_HUFFMANTREE_H
--- Problem29/main.cpp
+++ Problem29/main.cpp
@@ -11,8 +11,13 @@ int main()
         getline(file, text);
         file.close();
     }
-    buildHuffmanTree(text);
-    return 0;
+    buildHuffmanTree_encode(text);
+
+    map<char, int> text2 = { {'a',2 }, {'h', 3}, {'o', 1},
+                             {'n', 2}, {'g', 1}, {'m', 2},
+                             {'e', 1}, {'k', 1}, {'i', 1},
+                             {' ', 2}};
+    buildHuffmanTree_decode(text2,"1110001101001011001011110100111001101011101010100011");
 }
 
 
