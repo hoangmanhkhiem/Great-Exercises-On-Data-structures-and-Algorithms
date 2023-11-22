f56cf29fa249cb5c57e576b5bfa1e10915a64197
Hoàng Mạnh Khiêm <khiemhm04@gmail.com>
2023-11-22 23:24:52 +0700

    delete temp in app

--- app/Project29/test.py
+++
@@ -1,9 +0,0 @@
-freq_code = "ACc"
-frg_num = "1~2~3"
-frq = list(map(int, frg_num.split("~")))
-list_seg_code = ""
-for i in range(len(freq_code)):
-    for j in range(frq[i]):
-        list_seg_code += freq_code[i]
-
-print(list_seg_code)
